807797daf0d1b49627691077c6833bca083e9560
JiangChaoZhang <mac@MacdeiMac-161.local>
2016-03-12 15:28:26 +0800

    zjc
    zjcdemo

---
+++ WeekDayDemo/WeekDayDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeekDayDemo
+//
+//  Created by Mac on 16/3/12.
+//  Copyright © 2016年 张江朝. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeekDayDemo/WeekDayDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeekDayDemo
+//
+//  Created by Mac on 16/3/12.
+//  Copyright © 2016年 张江朝. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
